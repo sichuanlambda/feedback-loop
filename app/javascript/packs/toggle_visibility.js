1c142f16d6264caf414dc4837f74797da6f08e7f
sichuanlambda <cdnathan.robinson@gmail.com>
2023-10-26 17:26:49 +0200

    inclusion of javascript for visibility toggling

---
+++ app/javascript/packs/toggle_visibility.js
@@ -0,0 +1,9 @@
+function toggleVisibility() {
+    var text = document.getElementById("hiddenText");
+    if (text.style.display === "none") {
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }
+  
