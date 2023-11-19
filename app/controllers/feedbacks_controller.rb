d1cb11a40efb14b5106f91ea9e838c9ac840ed62
sichuanlambda <cdnathan.robinson@gmail.com>
2023-11-19 16:58:58 -0700

    added retry to GPT call, storing input & outputs in db

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -44,9 +44,22 @@ class FeedbacksController < ApplicationController
 
   # everything below (except final end) is for the integraiton with GPT
   def ask_gpt
+    start_time = Time.current  # Capture the start time
+
     prompt_response = GptService.new.send_prompt(params[:prompt])
+
     if prompt_response
-      # Assuming the response is a string containing the AI's message
+      end_time = Time.current  # Capture the end time
+      response_time = ((end_time - start_time) * 1000).to_i  # Calculate response time in milliseconds
+
+      # Create a new GptInteraction record
+      GptInteraction.create(
+        submitted_at: start_time,
+        response_time: response_time,
+        user_input: params[:prompt],
+        gpt_response: prompt_response
+      )
+
       render json: { text: prompt_response }
     else
       render json: { error: 'No response from GPT service' }, status: :bad_request
---
+++ test/models/gpt_interaction_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class GptInteractionTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
