5e3c6d145406f1e52d7fb6f1d56f8c5de44d8016
sichuanlambda <cdnathan.robinson@gmail.com>
2023-11-26 01:29:53 -0700

    Update AWS initializer for Heroku

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -2,15 +2,20 @@
 
 require 'aws-sdk-textract'
 
-# Debugging: Output the credentials being loaded
-puts "AWS Region from credentials: #{Rails.application.credentials.dig(:aws, :region)}"
-puts "AWS Access Key ID from credentials: #{Rails.application.credentials.dig(:aws, :access_key_id)}"
+# Prefer environment variables, fallback to credentials file if not found
+aws_region = ENV['AWS_REGION'] || Rails.application.credentials.dig(:aws, :region)
+aws_access_key_id = ENV['AWS_ACCESS_KEY_ID'] || Rails.application.credentials.dig(:aws, :access_key_id)
+aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY'] || Rails.application.credentials.dig(:aws, :secret_access_key)
+
+# Debugging: Output the credentials being loaded (remove these lines in production)
+puts "AWS Region from credentials: #{aws_region}"
+puts "AWS Access Key ID from credentials: #{aws_access_key_id}"
 puts "AWS Secret Access Key from credentials: [REDACTED]" # Do not print the secret access key
 
 Aws.config.update({
-  region: Rails.application.credentials.aws[:region],
+  region: aws_region,
   credentials: Aws::Credentials.new(
-    Rails.application.credentials.aws[:access_key_id],
-    Rails.application.credentials.aws[:secret_access_key]
+    aws_access_key_id,
+    aws_secret_access_key
   )
 })
