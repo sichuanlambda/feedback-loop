b9f9043e9aec82f73a672734e0e1cc16a921cecb
sichuanlambda <cdnathan.robinson@gmail.com>
2023-11-28 20:39:01 -0700

    bin/rails db:drop db:create db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_29_012113) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_29_023632) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -57,13 +57,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_29_012113) do
   end
 
   create_table "screenshot_analyses", force: :cascade do |t|
-    t.text "extracted_text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "screenshots", force: :cascade do |t|
-    t.string "image"
     t.text "extracted_text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
