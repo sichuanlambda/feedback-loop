7c4cd5d9329d8f98b139f6586569b3a7ca22d1a7
sichuanlambda <cdnathan.robinson@gmail.com>
2023-12-06 17:31:04 -0700

    Updating roastery to save to db again

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -105,8 +105,22 @@ class FeedbacksController < ApplicationController
   end
 
   def ask_gpt
+    start_time = Time.current  # Capture the start time
+
     prompt_response = GptService.new.send_prompt(params[:prompt])
+
     if prompt_response
+      end_time = Time.current  # Capture the end time
+      response_time = ((end_time - start_time) * 1000).to_i  # Calculate response time in milliseconds
+
+      # Create a new GptInteraction record
+      GptInteraction.create(
+        submitted_at: start_time,
+        response_time: response_time,
+        user_input: params[:prompt],
+        gpt_response: prompt_response
+      )
+
       render json: { text: prompt_response }
     else
       render json: { error: 'No response from GPT service' }, status: :bad_request
