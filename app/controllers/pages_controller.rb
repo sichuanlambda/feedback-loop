bb3395d28e0e0c8dd4ead0263b97ca790086a68e
sichuanlambda <cdnathan.robinson@gmail.com>
2024-01-31 18:41:01 -0700

    link to profile pages from account page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user! # If you're using Devise
   def home
+  end
+
+  def account
+    @user = current_user # This sets the @user variable to the currently logged in user
   end
 end
