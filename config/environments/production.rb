52d797c08697bcf73a20b9c925daa0fb1040505e
sichuanlambda <cdnathan.robinson@gmail.com>
2024-02-16 22:14:44 -0700

    v1.1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
     address: 'smtp.brevo.com',
     port: 587,
     domain: 'architecturehelper.com',
-    user_name: Rails.application.credentials.smtp_settings[:'nathaninproduct@gmail.com'],
+    user_name: 'nathaninproduct@gmail.com', # Hardcoded email
     password: Rails.application.credentials.smtp_settings[:password],
     authentication: 'login',
     enable_starttls_auto: true
