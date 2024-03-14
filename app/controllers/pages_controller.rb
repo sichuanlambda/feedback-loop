c7ccadba5078e70f0ad5c5ccc4d6f436c14cd663
sichuanlambda <cdnathan.robinson@gmail.com>
2024-03-13 20:25:42 -0600

    Accounts page update UI updates

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,23 @@
 class PagesController < ApplicationController
   before_action :authenticate_user! # If you're using Devise
+  before_action :set_user_stats, only: [:account]
+
   def home
   end
 
   def account
-    @user = current_user # This sets the @user variable to the currently logged in user
+    @user = current_user
+    # Any other setup needed for the account page can be added here
+  end
+
+  private
+
+  def set_user_stats
+    return unless user_signed_in?
+
+    style_frequency = BuildingAnalysis.style_frequency(current_user.id)
+    @style_frequency = style_frequency.to_a.sort_by { |_, frequency| -frequency }.first(5)
+    @unique_style_count = style_frequency.length
+    @buildings_submitted_count = BuildingAnalysis.where(user: current_user).count
   end
 end
