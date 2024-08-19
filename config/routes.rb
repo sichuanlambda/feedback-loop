17e9e372d91ea8c66e5065b0c8e31c1347646c58
sichuanlambda <cdnathan.robinson@gmail.com>
2024-08-18 19:45:00 -0600

    Routes update for searches to work

--- config/routes.rb
+++ config/routes.rb
@@ -39,7 +39,7 @@ Rails.application.routes.draw do
   get 'proxy/fetch_street_view', to: 'proxy#fetch_street_view'
 
   # Routes for SearchesController
-  resources :searches, only: [:new, :create]
+  resources :searches, only: [:new, :create, :index]
 
   # Architecture Explorer routes
   get 'architecture_explorer/address_search', to: 'architecture_explorer#address_search'
