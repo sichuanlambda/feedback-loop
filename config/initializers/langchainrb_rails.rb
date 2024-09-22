b6ff397153f13b036cb95604733403c55eecc006
sichuanlambda <cdnathan.robinson@gmail.com>
2024-09-22 23:01:21 +0200

    removing logger

--- config/initializers/langchainrb_rails.rb
+++ config/initializers/langchainrb_rails.rb
@@ -2,7 +2,6 @@
 
 LangchainrbRails.configure do |config|
   api_key = ENV['OPENAI_API_KEY']
-  Rails.logger.info "OpenAI API Key: #{api_key}"
   config.vectorsearch = Langchain::Vectorsearch::Pgvector.new(
     llm: Langchain::LLM::OpenAI.new(api_key: api_key)
   )
