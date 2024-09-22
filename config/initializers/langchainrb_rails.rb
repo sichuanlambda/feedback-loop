720e5df1136fa3a5b5c4a5ef751de4d3c90d0b94
sichuanlambda <cdnathan.robinson@gmail.com>
2024-09-22 22:57:51 +0200

    Add debug log for OpenAI API key

--- config/initializers/langchainrb_rails.rb
+++ config/initializers/langchainrb_rails.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 LangchainrbRails.configure do |config|
+  api_key = ENV['OPENAI_API_KEY']
+  Rails.logger.info "OpenAI API Key: #{api_key}"
   config.vectorsearch = Langchain::Vectorsearch::Pgvector.new(
-    llm: Langchain::LLM::OpenAI.new(api_key: ENV["OPENAI_API_KEY"])
+    llm: Langchain::LLM::OpenAI.new(api_key: api_key)
   )
 end
