abbac5683faa44f688aa4ffc9ee38d435d3c4942
sichuanlambda <cdnathan.robinson@gmail.com>
2024-10-06 18:38:54 +0200

    openai creds & design "show" page button fix

--- config/initializers/langchainrb_rails.rb
+++ config/initializers/langchainrb_rails.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 LangchainrbRails.configure do |config|
-  api_key = ENV['OPENAI_API_KEY']
+  api_key = Rails.application.credentials.openai[:access_token]
   config.vectorsearch = Langchain::Vectorsearch::Pgvector.new(
     llm: Langchain::LLM::OpenAI.new(api_key: api_key)
   )
