b97a74a0d02d8e246266384f8d4a7851ad7c53e7
sichuanlambda <cdnathan.robinson@gmail.com>
2025-02-08 21:55:23 +0100

    and removing the aws creds fromp printing

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,15 +1,9 @@
-
 require 'aws-sdk-textract'
 
 # Prefer environment variables, fallback to credentials file if not found
 aws_region = ENV['AWS_REGION'] || Rails.application.credentials.dig(:aws, :region)
 aws_access_key_id = ENV['AWS_ACCESS_KEY_ID'] || Rails.application.credentials.dig(:aws, :access_key_id)
 aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY'] || Rails.application.credentials.dig(:aws, :secret_access_key)
-
-# Debugging: Output the credentials being loaded (remove these lines in production)
-puts "AWS Region from credentials: #{aws_region}"
-puts "AWS Access Key ID from credentials: #{aws_access_key_id}"
-puts "AWS Secret Access Key from credentials: [REDACTED]" # Do not print the secret access key
 
 Aws.config.update({
   region: aws_region,
