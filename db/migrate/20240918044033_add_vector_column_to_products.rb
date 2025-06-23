cdd5e07078ddb3085da892624b4db08ebaea36a3
sichuanlambda <cdnathan.robinson@gmail.com>
2025-06-23 17:46:18 +0200

    updated creds

--- db/migrate/20240918044033_add_vector_column_to_products.rb
+++
@@ -1,8 +0,0 @@
-class AddVectorColumnToProducts < ActiveRecord::Migration[7.1]
-  def change
-    # Define the default dimension before the migration runs
-    default_dimension = Langchain::LLM::OpenAI.new(api_key: Rails.application.credentials.openai[:api_key]).default_dimension
-
-    add_column :products, :embedding, :vector, limit: default_dimension
-  end
-end
